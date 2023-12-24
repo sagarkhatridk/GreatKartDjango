1384eeebeddf9d2735a83d6e9b5da361551514b7
sagarkhatridk <sagarkhatridk@gmail.com>
2023-12-24 21:24:06 +0530

    section 13 completed

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -139,3 +139,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+
+#  ! Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sagarkhatridk@gmail.com'
+EMAIL_HOST_PASSWORD = 'lougxhvhvvlqbswx'
+EMAIL_USE_TLS = True
