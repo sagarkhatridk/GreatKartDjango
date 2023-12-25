a3f29bddd82709ed68a82ff18e5d340150b1a2f2
sagarkhatridk <sagarkhatridk@gmail.com>
2023-12-25 22:14:10 +0530

    section 15:1 completed

--- carts/views.py
+++ carts/views.py
@@ -102,16 +102,20 @@ def remove_cart_item(request, product_id, cart_item_id):
 
 
 def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
 
-    cart = Cart.objects.get(cart_id=_cart_id(request))
-    cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
 
-    for cart_item in cart_items:
-        total += (cart_item.product.price * cart_item.quantity)
-        quantity += cart_item.quantity
-
-    tax = (2 * total)/100
-    grand_total = total + tax
+        tax = (2 * total)/100
+        grand_total = total + tax
+    except Exception as E:
+        pass
 
 
     context = {
@@ -123,3 +127,29 @@ def cart(request, total=0, quantity=0, cart_items=None):
     }
 
     return render(request, 'store/cart.html', context)
+
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+        tax = (2 * total)/100
+        grand_total = total + tax
+    except Exception as E:
+        pass
+
+
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+        'tax':tax,
+        'grand_total':grand_total,
+    }
+    return render(request, 'store/checkout.html', context)
