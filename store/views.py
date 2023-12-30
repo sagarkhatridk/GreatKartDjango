cb68a1465985ada72e95f0c698acd925eead34e5
sagarkhatridk <sagarkhatridk@gmail.com>
2023-12-30 13:26:28 +0530

    section 19 completed

--- store/views.py
+++ store/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .forms import ReviewForm
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Product, ReviewRating
 from category.models import Category
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
 from django.db.models import Q
-
+from django.contrib import messages
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
@@ -63,3 +63,26 @@ def search(request):
     }
 
     return render(request, 'store/store.html', context)
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you! Your review has been updated')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thank you! Your review has been updated')
+                return redirect(url)
